b524de689476ced345f8e195bde0384108056787
Lais Santiago <santiago_lais@outlook.com>
2021-04-15 18:07:57 -0300

    Banco de Dados H2 e criacao automatica da base de dados

--- src/main/java/com/laissantiago/cursomc/domain/Categoria.java
+++ src/main/java/com/laissantiago/cursomc/domain/Categoria.java
@@ -1,12 +1,19 @@
 package com.laissantiago.cursomc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = -3510391839707867552L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
