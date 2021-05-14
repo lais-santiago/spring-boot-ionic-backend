823b84896ad3493e9c56a18694e6d0ec066db024
Lais Santiago <santiago_lais@outlook.com>
2021-05-14 17:32:45 -0300

    Paginacacao com parametros opcionais na requisicao

--- src/main/java/com/laissantiago/cursomc/CursomcApplication.java
+++ src/main/java/com/laissantiago/cursomc/CursomcApplication.java
@@ -50,6 +50,11 @@ public class CursomcApplication implements CommandLineRunner {
   public void run(String... args) throws Exception {
     var cat1 = new Categoria(null, "Informática");
     var cat2 = new Categoria(null, "Escritório");
+    var cat3 = new Categoria(null, "Cama mesa e banho");
+    var cat4 = new Categoria(null, "Eletrônicos");
+    var cat5 = new Categoria(null, "Jardinagem");
+    var cat6 = new Categoria(null, "Decoração");
+    var cat7 = new Categoria(null, "Perfumaria");
 
     var p1 = new Produto(null, "Computador", 2000.00);
     var p2 = new Produto(null, "Impressora", 800.00);
@@ -62,7 +67,7 @@ public class CursomcApplication implements CommandLineRunner {
     p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
     p3.getCategorias().addAll(Arrays.asList(cat1));
 
-    categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 
     produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
--- src/main/java/com/laissantiago/cursomc/services/CategoriaService.java
+++ src/main/java/com/laissantiago/cursomc/services/CategoriaService.java
@@ -6,6 +6,9 @@ import com.laissantiago.cursomc.services.exceptions.DataIntegrityException;
 import com.laissantiago.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -46,5 +49,11 @@ public class CategoriaService {
   
   public List<Categoria> findAll(){
     return repo.findAll();
+  }
+  
+  public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+    var pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+    
+    return repo.findAll(pageRequest);
   }
 }
